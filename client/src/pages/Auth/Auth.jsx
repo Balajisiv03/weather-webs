4175ac1af9ae0330a586866cb4897fa5dd2b1f0e
Balaji <balajisivakumar2003@gmail.com>
2024-01-29 12:54:34 +0530

    first commit

---
+++ client/src/pages/Auth/Auth.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Auth.css'
+
+
+const Auth = () => {
+
+  const[IsSignUp,setIsSignUp]=useState(false)
+  const[name,setName]=useState('');
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  return (
+     <section className='auth-section'>
+        <div className='auth-container-2'>
+          
+        </div>
+     </section>
+  )
+}
+
+export default Auth
