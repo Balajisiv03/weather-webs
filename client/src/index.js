62afb916b705037d2cae2fc710d2265da87de1e8
Balaji <balajisivakumar2003@gmail.com>
2024-02-01 19:17:08 +0530

    ui changes

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux';
 import {createStore,applyMiddleware,compose} from 'redux';
 import {thunk} from 'redux-thunk';
 import Reducers from './reducers';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store=createStore(Reducers,compose(applyMiddleware(thunk)))
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React,{useState,useEffect} from 'react'
+import waterimg from '../../assets/waterimg.jpg'
 import './Home.css';
 
 const Home = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+
   const [weatherData, setWeatherData] = useState(null);
   const[search,setSearch]=useState("");
-  // eslint-disable-next-line
-  const [timezone, setTimezone] = useState('');
-  const [error,setError]=useState(null)
+  const [currentTime, setCurrentTime] = useState(new Date());
+   // eslint-disable-next-line
+   const [timezone, setTimezone] = useState('');
+   const [error,setError]=useState(null)
 
 
-  const searchButton=()=>{
+  const handleSearch=()=>{
 
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${'58b6e640da732bb8d49f7d66b8c3ff44'}`
@@ -42,18 +44,6 @@ const Home = () => {
     navigator.geolocation.getCurrentPosition(async (position) => {
       const { latitude, longitude } = position.coords;
 
-      // const worldTimeApiUrl = `http://worldtimeapi.org/api/timezone`;
-    
-      // try {
-      //   const worldTimeResponse = await fetch(worldTimeApiUrl);
-      //   const worldTimeData = await worldTimeResponse.json();
-      //   const userTimezone = worldTimeData.timezone;
-      //   setTimezone(userTimezone);
-      // } catch (error) {
-      //   console.error('Error fetching user timezone:', error);
-      // }
-
-
       // Fetch weather information (using OpenWeatherMap API)
       const apiKey = '58b6e640da732bb8d49f7d66b8c3ff44';
       const apiResponse = await fetch(
@@ -62,14 +52,13 @@ const Home = () => {
       const data = await apiResponse.json();
       
 
-      // Update state with weather data
       setWeatherData(data);
       if(data.timezone){
         setTimezone(data.timezone);
       }
     });
 
-    // Update current time every second
+  
     const intervalId = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
@@ -78,6 +67,8 @@ const Home = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+
+  
   const getDynamicColor = () => {
     const currentHour = currentTime.getHours();
 
@@ -97,45 +88,83 @@ const Home = () => {
     if (weatherData && weatherData.weather && weatherData.weather[0]) {
       const weatherDescription = weatherData.weather[0].description.toLowerCase();
       if (weatherDescription.includes('clear')) {
-        return 'clear-sky-color';
+         return { className: 'clear-sky-color', backgroundImage: 'url(clearskyimg.jpg)' };
       } else if (weatherDescription.includes('cloud')) {
-        return 'cloudy-color';
+        return { className: 'cloudy-color', backgroundImage: 'url(cloudyimg.jpg)' };
       } else if (weatherDescription.includes('rain')) {
-        return 'rainy-color';
+        return { className: 'rainy-color', backgroundImage: 'url(rainyimg.jpg)' };
       }
     }
-    return 'default-color';
+    return { className: 'default-color', backgroundImage: 'url(waterimg.jpg)' };
   };
 
-  // Use the getDynamicColor and getWeatherColor functions to determine the background color dynamically
   const containerStyle = {
     backgroundColor: `${getDynamicColor()} ${getWeatherColor()}`,
   };
 
+
+let icon = null;
+
+if (weatherData && weatherData.main) {
+  if (weatherData.weather[0].main === "Clouds") {
+    icon = "fa-cloud";
+  } else if (weatherData.weather[0].main === "Thunderstorm") {
+    icon = "fa-bolt";
+  } else if (weatherData.weather[0].main === "Drizzle") {
+    icon = "fa-cloud-rain";
+  } else if (weatherData.weather[0].main === "Rain") {
+    icon = "fa-cloud-shower-heavy";
+  } else if (weatherData.weather[0].main === "Snow") {
+    icon = "fa-snow-flake";
+  } else {
+    icon = "fa-smog";
+  }
+} else {
+  return <div>Loading...</div>;
+}
+
   return (
-     <div className={`home-container ${getDynamicColor()} ${getWeatherColor()}`} style={containerStyle}>
-       <h1 style={{marginBottom:"100px"}}>Weather App</h1>
-       <div>
-         <input type="text" placeholder='Enter location' onChange={(e)=>{setSearch(e.target.value)}}/>
-         <button onClick={searchButton}>Search</button>
-       </div>
-      <div className="time"><p>Current time: {currentTime.toLocaleTimeString()}</p></div> 
-      {weatherData ? (
-        <div className="weather">
-          <p>{weatherData.name}, {weatherData.sys.country}</p>
-          <p>Temperature: {`${Math.round(weatherData.main.temp - 273.15)}°C`}</p>
-          <div>
-            <p>Latitude: {weatherData.coord.lat}</p>
-            <p>Longitude: {weatherData.coord.lon}</p>
-          </div>
-          <p>Weather: {`${weatherData.weather[0].description}`}</p>
+  <div className={`${getDynamicColor()} ${getWeatherColor()}`} style={containerStyle}>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className='col-md-4'>
+        <h2><p className=" time card-text">Current time: {currentTime.toLocaleTimeString()}</p></h2>
+            <div className="weather card text-white text-center border-0">
+                  <img src={waterimg} className="card-img" alt="..." width="300" height="600"/>
+                  <div className="card-img-overlay">
+                        <div className="input-group mb-4 w-95 mx-auto">
+                          <input type="search" className="form-control" placeholder="Type location" aria-label="Type location"
+                           aria-describedby="basic-addon2" onChange={(e)=>setSearch(e.target.value)}/>
+                          <button type="submit" className="input-group-text" id="basic-addon2" onClick={handleSearch}>
+                            <i className='fas fa-search'></i>
+                           </button>
+                        </div>
+
+                    {weatherData ? (
+                
+                        <div className='bg-black bg-opacity-50 py-3'>
+                          <h2 className="card-title">{weatherData.name}, {weatherData.sys.country}</h2>
+                          <p className="card-text">{currentTime.toDateString()}</p>
+                          <hr />
+                          <h2 className='card-title'>Temperature: {`${Math.round(weatherData.main.temp - 273.15)}°C`}</h2>  
+                          <i className={`fas ${icon} fa-5x`}></i>
+                          <h1 className='fw-bolder mb-1'>{weatherData.weather[0].description}</h1>       
+                          <p className="card-text mb-1"><small>Latitude: {weatherData.coord.lat}</small></p>         
+                          <p className="card-text"><small>Longitude: {weatherData.coord.lon}</small></p>         
+                          <p className='lead fw-bolder mb-0'>{weatherData.weather[0].main}</p>
+                          <p className='lead'>Min: {weatherData.main.temp_min} &deg;C | Max: {weatherData.main.temp_max} &deg;C</p>
+                        </div>
+                         ) :(
+                          <div>Loading Weather data</div>
+                         )}  
+                  </div>
+            </div>
         </div>
-      ) : (
-        <div>Loading weather data...</div>
-      )}
+      </div>
     </div>
-  );
-};
+  </div>
+   
+  )
+}
 
-export default Home;
-
+export default Home
